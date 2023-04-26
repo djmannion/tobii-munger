351307674d75f988a1f60548b62eb01be979a090
Damien Mannion <damien.mannion@unimelb.edu.au>
2023-04-26 12:50:05 +1000

    -draft conversion

--- src/tobii_munger/io.py
+++ src/tobii_munger/io.py
@@ -12,40 +12,6 @@ def _name_generator(ii: int) -> str:
     return "xyz"[ii]
 
 
-def _to_xarray(data: pl.DataFrame) -> dict:
-    "Converts the Polars DataFrame from `read_unified(as_xarray=False)` to `xarray` format"
-
-    def convert_timestamps(datatype):
-        timestamps_s = data.filter(predicate=pl.col("type") == datatype).select(exprs=pl.col("timestamp"))
-        timestamps_s = np.squeeze(np.array(timestamps_s))
-        # convert this to nanoseconds for xarray
-        timestamps_ns = (timestamps_s * 1e9).astype("timedelta64[ns]")
-        assert np.allclose(timestamps_ns / np.timedelta64(1, "s"), timestamps_s)
-        return timestamps_ns
-
-    gaze_timestamps = convert_timestamps(datatype="gaze2d")
-
-    # gaze is a dataset with dims time, camera_space, world_space, eye
-    # dvs are: gaze_camera, gaze_world, pupil_diameter, gaze_origin, gaze_direction
-    gaze_screen_data = data.filter(predicate=pl.col("type") == "gaze2d").select(pl.col("x", "y")).to_numpy()
-
-    gaze_screen = xr.DataArray(
-        data=gaze_screen_data,
-        dims=("time", "camera_space"),
-        coords={"time": gaze_time_ns, "camera_space": np.array(["x", "y"], dtype=str)},
-    )
-
-    return gaze_screen
-
-
-    n_gaze_samples = len(gaze_time)
-
-
-
-    return gaze_time
-
-
-
 def read_unified(path: Path | str, datatype: str | None = None, as_xarray: bool = False) -> pl.DataFrame | dict:
     """Read Tobii data from a unified parquet file.
 
@@ -71,3 +37,164 @@ def read_unified(path: Path | str, datatype: str | None = None, as_xarray: bool 
     if as_xarray:
         data = _to_xarray(data=data)
     return data
+
+
+def _to_xarray(data: pl.DataFrame) -> dict:
+    "Converts the Polars DataFrame from `read_unified(as_xarray=False)` to `xarray` format"
+
+    def convert_timestamps(datatype):
+        timestamps_s = data.filter(predicate=pl.col("type") == datatype).select(exprs=pl.col("timestamp"))
+        timestamps_s = np.squeeze(np.array(timestamps_s))
+        # convert this to nanoseconds for xarray
+        timestamps_ns = (timestamps_s * 1e9).astype("timedelta64[ns]")
+        assert np.allclose(timestamps_ns / np.timedelta64(1, "s"), timestamps_s)
+        return timestamps_ns
+
+    # gaze sample data
+
+    # dimensions and coordinates
+    # each tuple is (dim_name, dim_coords, dim_attrs)
+    # we will define the time dimension separately for each data array
+    norm_space_dim = (
+        "norm_space",
+        np.array(["x", "y"], dtype=str),
+        {"units": "Normalised video space; 0 is top/left, 1 is bottom/right"},
+    )
+    world_space_dim = (
+        "world_space",
+        np.array(["x", "y", "z"], dtype=str),
+        {"units": "Relative to the scene camera, in mm"},
+    )
+
+    # form data arrays
+    gaze_video = xr.DataArray(
+        data=data.filter(predicate=pl.col("type") == "gaze2d").select(pl.col("x", "y")).to_numpy(),
+        dims=("time", "norm_space"),
+        coords=(
+            ("time", convert_timestamps(datatype="gaze2d")),
+            norm_space_dim,
+        ),
+        attrs={
+            "info": "Position of gaze in the scene camera",
+        },
+    )
+
+    gaze_world = xr.DataArray(
+        data=data.filter(predicate=pl.col("type") == "gaze3d").select(pl.col("x", "y", "z")).to_numpy(),
+        dims=("time", "world_space"),
+        coords=(
+            ("time", convert_timestamps(datatype="gaze3d")),
+            world_space_dim,
+        ),
+        attrs={
+            "info": "Position of the vergence point of the left and right gaze vector relative to the scene camera.",
+        },
+    )
+
+    # these are per-eye, so need to iterate over eyes
+    gaze_origin = []
+    gaze_direction = []
+    pupil_diameter = []
+
+    for eye in ["left", "right"]:
+
+        gaze_origin.append(
+            xr.DataArray(
+                data=data.filter(predicate=pl.col("type") == f"eye{eye}|gazeorigin").select(pl.col("x", "y", "z")).to_numpy()[..., np.newaxis],
+                dims=("time", "world_space", "eye"),
+                coords=(
+                    ("time", convert_timestamps(datatype=f"eye{eye}|gazeorigin")),
+                    world_space_dim,
+                    ("eye", np.array([eye], dtype=str)),
+                ),
+                attrs={
+                    "info": "Position of the eye relative to the scene camera",
+                },
+            )
+        )
+
+        gaze_direction.append(
+            xr.DataArray(
+                data=data.filter(predicate=pl.col("type") == f"eye{eye}|gazedirection").select(pl.col("x", "y", "z")).to_numpy()[..., np.newaxis],
+                dims=("time", "world_space", "eye"),
+                coords=(
+                    ("time", convert_timestamps(datatype=f"eye{eye}|gazedirection")),
+                    world_space_dim,
+                    ("eye", np.array([eye], dtype=str)),
+                ),
+                attrs={
+                    "info": "The estimated gaze direction in 3D. The origin of this vector is the gazeorigin for the respective eye"
+                },
+            )
+        )
+
+        pupil_diameter.append(
+            xr.DataArray(
+                data=data.filter(predicate=pl.col("type") == f"eye{eye}|pupildiameter").select(pl.col("x")).to_numpy(),
+                dims=("time", "eye"),
+                coords=(
+                    ("time", convert_timestamps(datatype=f"eye{eye}|pupildiameter")),
+                    ("eye", np.array([eye], dtype=str)),
+                ),
+            )
+        )
+
+    gaze_data = xr.Dataset(
+        data_vars={
+            "gaze_video": gaze_video,
+            "gaze_world": gaze_world,
+            "gaze_origin": xr.concat(objs=gaze_origin, dim="eye"),
+            "gaze_direction": xr.concat(objs=gaze_direction, dim="eye"),
+            "pupil_diameter": xr.concat(objs=pupil_diameter, dim="eye"),
+        },
+    )
+
+    # IMU data
+
+    # the accelerometer and gyroscope seem to operate on the same clock, so represent those
+    # in the same dataset and the magnetometer in its own array
+    imu_accel_gyro_data = xr.Dataset(
+        data_vars={
+            imu_type: xr.DataArray(
+                data=data.filter(predicate=pl.col("type") == imu_type).select(pl.col("x", "y", "z")).to_numpy(),
+                dims=("time", "world_space"),
+                coords=(
+                    ("time", convert_timestamps(datatype=imu_type)),
+                    world_space_dim,
+                ),
+                attrs=imu_attrs,
+            )
+            for (imu_type, imu_attrs) in {
+                "accelerometer": {
+                    "units": "m / s^2",
+                    "info": "Acceleration in three dimensions in the coordinate system of the head unit",
+                },
+                "gyroscope": {
+                    "units": "deg / s",
+                    "info": "Rotation of the head unit",
+                },
+            }.items()
+        },
+    )
+
+    imu_magnetometer_data = xr.DataArray(
+        data=data.filter(predicate=pl.col("type") == "magnetometer").select(pl.col("x", "y", "z")).to_numpy(),
+        dims=("time", "world_space"),
+        coords=(
+            ("time", convert_timestamps(datatype="magnetometer")),
+            world_space_dim,
+        ),
+        attrs={
+            "units": "microtesla",
+            "info": "Magnetic field near the head unit",
+        },
+    )
+
+    xr_data = {
+        "gaze": gaze_data,
+        "imu_accel_gyro": imu_accel_gyro_data,
+        "imu_magnetometer": imu_magnetometer_data,
+    }
+
+    return xr_data
+
